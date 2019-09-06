ab1934d9fb0b0efc6ff61dcff476bb81f01bbda5
Danter28 <38162330+Danter28@users.noreply.github.com>
2019-09-06 15:04:40 +0000

    Update Cama.cpp

--- Cama.cpp
+++ Cama.cpp
@@ -3,4 +3,19 @@
 //
 
 #include "Cama.h"
+Cama::Cama(){}
+
+Cama::Cama(const std::string& codigo, bool estado):codigo(codigo),estado(estado){}
+
+Cama::~Cama(){}
+
+const std::string& Cama::getCodigo() {}
+
+void Cama:: setCodigo(const std::string& codigo){}
+
+bool Cama:: isEstado() const{}
+
+void Cama:: setEstado(bool estado){}
+
+std::string Cama::toString() {}
 
